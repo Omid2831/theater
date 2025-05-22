cd00e1198e64caf8242fb195b3ce66a44cc61173
Omid <mehrabiomid8282@gmail.com>
2025-05-22 07:39:53 +0200

    remove the bugs

--- app/controllers/Tickets.php
+++ app/controllers/Tickets.php
@@ -5,32 +5,15 @@ class Tickets extends BaseController
 
     public function index($firstname = NULL, $infix = NULL, $lastname = NULL)
     {
-        // // start de sessie
-        session_start();
-
-        // // // check of de bezoeker is ingelogd
-        if (!isset($_SESSION['bezoekerId'])) {
-            echo "Je moet ingelogd zijn om deze pagina te bekijken.";
-            exit();
-        } 
-
-            $bezoekerId = $_SESSION['bezoekerId'];
-
-            // Load the Ticket model and fetch tickets for this bezoeker
-            require_once APPROOT . '/Models/TicketModel.php';
-            $tickets = Ticket::getTicketsForBezoeker($bezoekerId);
         
-        /**
-         * Het $data-array geeft informatie mee aan de view-pagina
-         */
         $data = [
             'title' => 'Mijn Tickets',
-            'tickets' => $tickets, // Uncomment this line if you want to pass tickets to the view
+           
         ];
-        /**
-         * Met de view-method uit de BaseController-class wordt de view
-         * aangeroepen met de informatie uit het $data-array
-         */
+
         $this->view('tickets/index', $data);
     }
+
+
 }
+
