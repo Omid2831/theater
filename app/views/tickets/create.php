978a49e3ba1c307d7aaa462ec7be20254d68fcb3
Omid <mehrabiomid8282@gmail.com>
2025-07-03 12:48:59 +0200

    refactor: rename input field for seat number to improve clarity and consistency

--- app/views/tickets/create.php
+++ app/views/tickets/create.php
@@ -72,10 +72,10 @@
                 <div class="mb-3">
                     <label for="Nummer" class="form-label">Stoel</label>
                     <input
-                        name="stoel"
+                        name="Number"
                         type="number"
                         class="form-control"
-                        id="stoel"
+                        id="Number"
                         value="<?= $_POST['Nummer'] ?? '' ?>"
                         min="1" max="100"
                         minlength="1" maxlength="3"
