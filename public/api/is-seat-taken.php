51c12d7082ffa2094a9c81bf406f920525fd846d
Omid <mehrabiomid8282@gmail.com>
2025-07-03 12:50:08 +0200

    refactor: remove error reporting settings for cleaner code

--- public/api/is-seat-taken.php
+++ public/api/is-seat-taken.php
@@ -1,9 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once '../../app/Models/TicketModel.php'; // Adjust path as needed
+require_once '../../app/Models/TicketModel.php'; 
 
 header('Content-Type: application/json');
 
